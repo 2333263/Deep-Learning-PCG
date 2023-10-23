b8a17d1be8808ce24e9081a9780d0679244bd97f
2333263 <2333263@students.wits.ac.za>
2023-10-23 18:36:41 +0200

    graphs being prepared

--- testModel.py
+++ testModel.py
@@ -5,6 +5,14 @@ import numpy as np
 import tensorflow as tf
 import copy
 import AStar
+#from tqdm import tqdm,trange
+import sys
+
+if(len(sys.argv)!=2):
+    print("Usage: python3 testModel.py <modelName>")
+    exit()
+else:
+    modelName=str(sys.argv[1])
 def printLevel(level):
     currLevel=level.reshape(30,45)
     currLevel=np.array(currLevel,dtype=int)
@@ -24,16 +32,22 @@ def getLevelDifference(level1,level2):
     return(diffRgb)
 
 
-def getFitness(level):
-    curr=copy.deepcopy(level.reshape(30,45))
+def getFitness(level, forceValid):
+    curr=copy.deepcopy(level)
+    curr=curr.reshape(30,45)
     floor=np.ones(45)
     fitness=0
     curr[len(curr)-1]=floor 
-    if(curr[27][0]!=0 or curr[27][44]!=0 or curr[28][44]!=1 or curr[28][0]!=1): #ensures that the start location and end location is valid
+    if forceValid:
+        curr[27][0]=0
+        curr[27][44]=0
+        curr[28][44]=1
+        curr[28][0]=1
+        #or curr[28][44]!=1 or curr[28][0]!=1
+    if(curr[27][0]!=0 or curr[27][44]!=0 ): #ensures that the start location and end location is valid
         return(-120,())
     else:
         path,dif=AStar.AStar(curr,27,0,27,44)
-
         if(dif==-1):
             fitness=-AStar.dist(path,(27,44))
         else:
@@ -49,8 +63,17 @@ def updateLevel(level,changes):
                 level[i]=0
     return level
 
-
+def RoundLevel(level):
+    for i in range(len(level)):
+        if(level[i]>=0.5):
+            level[i]=1
+        else:
+            level[i]=0
+            
+    return level
 def GetChunks(Level,startRow,endRow,startCol,endCol):
+    copyLevel=copy.deepcopy(Level)
+    copyLevel=copyLevel.reshape(30,45)
     chunks=[]
      
     for i in range(startRow,endRow):
@@ -59,7 +82,7 @@ def GetChunks(Level,startRow,endRow,startCol,endCol):
             for k in range(i-6,i+6):
                 for l in range(j-6,j+6):
                     if(k>=0 and k<30 and l>=0 and l<45):
-                        newLevel[k-i+6][l-j+6]=Level[k][l]
+                        newLevel[k-i+6][l-j+6]=copyLevel[k][l]
             chunks.append(newLevel)
     '''
     for i in range(len(Level)):
@@ -81,42 +104,158 @@ def GetChunks(Level,startRow,endRow,startCol,endCol):
     return chunks
 
 
+def getStack(Level,startRow,endRow,startCol,endCol):
+    copyLevel=copy.deepcopy(Level)
+    copyLevel=copyLevel.reshape(30,45)
+    stacks=[]
+     
+    for row in range(startRow,endRow):
+        for col in range(startCol,endCol):
+            newLevel=np.zeros((12,12))
+            i_min,i_max=max(0,row-6),min(len(copyLevel),row+6)
+            j_min,j_max=max(0,col-6),min(len(copyLevel[row]),col+6) 
+            
+            row_slice=slice(i_min-row+6,i_max-row+6)
+            col_slice=slice(j_min-col+6,j_max-col+6)
+            
+            newLevel[row_slice,col_slice]=copyLevel[i_min:i_max,j_min:j_max]
+            Stack=np.zeros((30,45,2),dtype=bool)
+            Stack[:,:,0]=copyLevel
+            newLevel=np.pad(newLevel,((9,9),(16,17)),"constant",constant_values=0)
+            Stack[:,:,1]=newLevel
+            stacks.append(Stack)
+    return stacks
+
+
 def getModel(modelName):
     modelLoc="Models/"+modelName
     model=tf.keras.models.load_model(modelLoc)
     return model
 
+def applyChunkChange(level,chunks,model):
+    chunk=0
+    copyLevel=copy.deepcopy(level)
+    for row in range(len(level)):
+        for col in range(len(level[0])):
+            change=model(chunks[chunk].reshape(1,12,12,1)).numpy()[0]
+            change=np.argmax(change)
+            if(change==0):
+                copyLevel[row][col]=0
+            elif(change==1):
+                copyLevel[row][col]=1
+            chunk+=1
+    return copyLevel
+
+def applyStackedChange(level,stacks,model):
+    pos=0
+    copyLevel=copy.deepcopy(level)
+    for row in range(len(level)):
+        for col in range(len(level[0])):
+            change=model(stacks[pos].reshape(1,30,45,2)).numpy()[0]
+            change=np.argmax(change)
+            if(change==0):
+                copyLevel[row][col]=0
+            elif(change==1):
+                copyLevel[row][col]=1
+            pos+=1
+    return copyLevel
+
+
+def applyComplexChange(level,chunks,model):
+    copyLevel1=copy.deepcopy(level)
+    copyLevel1=copyLevel1.reshape(1,30,45)
+    
+    copyLevel2=copy.deepcopy(level)
+    copyLevel2=copyLevel2.reshape(30,45)
+    chunkPos=0
+    for row in range(len(copyLevel2)):
+        for col in range(len(copyLevel2[0])):
+            change=model([np.array(copyLevel1),np.array(chunks[chunkPos].reshape(1,12,12)),np.array([row,col]).reshape(1,2)]).numpy()[0]
+            change=np.argmax(change)
+            if(change==0):
+                copyLevel2[row][col]=0
+            elif(change==1):
+                copyLevel2[row][col]=1
+            chunkPos+=1
+            
+    return copyLevel2
+
+def applyChangeCNN(level,model):
+    copyLevel=copy.deepcopy(level)
+    currLevel=copyLevel.reshape(1,30,45)
+    output=model(currLevel)
+    output=output.numpy()
+    output=output[0]
+    output=RoundLevel(output)
+    output=output.reshape(30,45)
+    return output
+
+def applyChangeStandardNN(level,model):
+    copyLevel=copy.deepcopy(level)
+    currLevel=copyLevel.reshape(1,30*45)
+    output=model(currLevel)
+    output=output.numpy()
+    output=output[0]
+    output=RoundLevel(output)
+    output=output.reshape(30,45)
+    return output
 def main():
-    #modelName="StandardNN"
-    startLevel=np.random.randint(0,2,size=(30,45))
-    
-    
-    floor=np.ones(45)
-    copyLevel=copy.deepcopy(startLevel)
-    copyLevel[len(copyLevel)-1]=floor
-    
-    diffLevel=getLevelDifference(startLevel,copyLevel)
-    
-    
-    fig,axes=plt.subplots(1,3)
-    ax=axes.ravel()
-    ax[0].imshow(startLevel,cmap="gray")
-    ax[0].set_title("Random Level")
-    ax[0].axis("off")
-    
-    ax[1].imshow(copyLevel,cmap="gray")
-    ax[1].set_title("Level are x many generations")
-    ax[1].axis("off")
-    
-    ax[2].imshow(diffLevel)
-    ax[2].set_title("Difference")
-    ax[2].axis("off")
-    
-    plt.show()
-   # model=getModel(modelName)
-   # level=copy.deepcopy(startLevel)
-    
-    
-    
+    global modelName
+    #iterations=1
+    model=getModel(modelName)
+    forceValid=False
+    for valid in range(2):
+        if(valid==1):
+            forceValid=True
+        for iterations in [1,2,3,4,5,10,100,1000,10000]:
+            fitness=[]
+            
+            startLevel=np.random.randint(0,2,size=(30,45))
+            fig,axes=plt.subplots(1,3,figsize=(15,5))
+            ax=axes.ravel()
+            fitness.append(getFitness(startLevel,forceValid)[0])
+            currLevel=startLevel.reshape(30,45)
+
+            ax[0].imshow(1-startLevel,cmap="gray")
+            ax[0].set_title("Original Level")
+            ax[0].axis("off")
+            
+            #for i in trange(iterations):
+            for i in range(iterations):
+                if(modelName=="DQN_Balanced_All_Elements" or modelName=="DQN_Unbalanced_All_Elements"):
+                    output=applyChunkChange(currLevel,GetChunks(currLevel,0,30,0,45),model)
+                elif(modelName=="StandardNN"):
+                    output=applyChangeStandardNN(currLevel,model)
+                elif(modelName=="CNN"):
+                    output=applyChangeCNN(currLevel,model)
+                elif(modelName=="DQN_Balanced_All_Elements_Stacked" or modelName=="DQN_Unbalanced_All_Elements_Stacked"):
+                    output=applyStackedChange(currLevel,getStack(currLevel,0,30,0,45),model)
+                elif(modelName=="ComplexModel"):
+                    output=applyComplexChange(currLevel,GetChunks(currLevel,0,30,0,45),model)
+                fitness.append(getFitness(output,forceValid)[0])
+                currLevel=output.reshape(1,30*45)
+            
+            
+            ax[1].imshow(1-output.reshape(30,45),cmap="gray")
+            ax[1].set_title(str(iterations)+" Iterations of Generation")
+            ax[1].axis("off")
+            
+            diff=getLevelDifference(startLevel,output.reshape(30,45))
+            
+            ax[2].imshow(diff)
+            ax[2].set_title("Difference")
+            ax[2].axis("off")
+            
+        # plt.show()
+            plt.savefig("Level Graphs/"+modelName+"/"+str(iterations)+"_Iterations_"+str(forceValid)+"_Valid")
+            plt.title("Difference between original and generated level after "+str(iterations)+" iterations")
+            plt.close()
+            
+            plt.plot(fitness)
+            plt.title("Fitness After "+str(iterations)+" Iterations")
+            plt.xlabel("Generation")
+            plt.ylabel("Fitness")
+            plt.savefig("Level Graphs/"+modelName+"/Fitness_"+str(iterations)+"_Iterations"+str(forceValid)+"_Valid")
+            plt.close()
 if __name__=="__main__":
     main()
